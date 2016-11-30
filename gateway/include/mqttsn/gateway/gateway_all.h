f8a4925ae90d06c593d614a4f2a87d1933572be4
Alex Robenko <arobenko@gmail.com>
2016-12-01 07:05:29 +1000

    Documentation for Gateway object.

--- gateway/include/mqttsn/gateway/gateway_all.h
+++ gateway/include/mqttsn/gateway/gateway_all.h
@@ -15,6 +15,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+/// @file
+/// @brief C interface for MQTT-SN Gateway library.
 
 #pragma once
 
@@ -55,11 +57,34 @@ typedef struct
     const char* password;
 } MqttsnAuthInfo;
 
+/// @brief Handle for configuration object used in all @b mqttsn_gw_config_* functions.
 typedef void* MqttsnConfigHandle;
+
+/// @brief Handle for gateway object used in all @b mqttsn_gw_* functions.
 typedef void* MqttsnGatewayHandle;
+
+/// @brief Handle for session object used in all @b mqttsn_gw_session_* functions.
 typedef void* MqttsnSessionHandle;
 
+/// @brief Type of callback function, to be used to request time measurement for
+///     the @b Gateway object.
+/// @details The callback is set using mqttsn_gw_set_tick_req_cb(). When
+///     the required time expires, the driving code is responsible to invoke
+///     mqttsn_gw_tick() function.
+/// @param[in] userData User data passed as the last parameter to the setting function.
+/// @param[in] duration Requested time measurement duration in @b milliseconds.
 typedef void (*MqttsnGwTickReqCb)(void* userData, unsigned duration);
+
+/// @brief Type of callback function, to be used to request broadcast of serialised
+///     @b ADVERTISE message.
+/// @details The callback is set using mqttsn_gw_set_advertise_broadcast_req_cb().
+/// @param[in] userData User data passed as the last parameter to the setting function.
+/// @param[in] buf Pointer to the buffer of data, that needs to be sent.
+/// @param[in] bufLen Number of bytes in the buffer.
+/// @note The provided buffer resides in private data structres of the @ref Gateway
+///     object and can be updated right after the callback function returns. The
+///     driving code may require to copy the buffer to its own data structures
+///     and preserve it intact until send over I/O link is complete.
 typedef void (*MqttsnGwBroadcastReqCb)(void* userData, const unsigned char* buf, unsigned bufLen);
 
 typedef void (*MqttsnSessionTickReqCb)(void* userData, unsigned duration);
@@ -109,17 +134,63 @@ unsigned mqttsn_gw_config_get_value(
     char* buf,
     unsigned bufLen);
 
+/// @brief Allocate @b Gateway object.
+/// @details The returned handle need to be passed as first parameter
+///     to all relevant functions. Note that the @b Gateway object is
+///     dynamically allocated and needs to be freed using
+///     mqttsn_gw_free() function.
+/// @return Handler to the allocated @b Gateway object.
 MqttsnGatewayHandle mqttsn_gw_alloc(void);
+
+/// @brief Free allocated @b Gateway object.
+/// @param[in] gw Handle returned by mqttsn_gw_alloc() function.
 void mqttsn_gw_free(MqttsnGatewayHandle gw);
+
+/// @brief Set the advertise period.
+/// @param[in] gw Handle returned by mqttsn_gw_alloc() function.
+/// @param[in] value Advertise period in @b seconds.
 void mqttsn_gw_set_advertise_period(MqttsnGatewayHandle gw, unsigned short value);
+
+/// @brief Set the numeric gateway ID.
+/// @param[in] gw Handle returned by mqttsn_gw_alloc() function.
+/// @param[in] id Numeric gateway ID.
 void mqttsn_gw_set_id(MqttsnGatewayHandle gw, unsigned char id);
+
+/// @brief Set callback that requests to perform time measurement.
+/// @details The @ref Gateway object will invoke the callback to request time
+///     measurement. When requested time expires, the driving code is responsible
+///     to call mqttsn_gw_tick().
+/// @param[in] gw Handle returned by mqttsn_gw_alloc() function.
+/// @param[in] cb Pointer to callback function
+/// @param[in] data Any user data, to be passed as first parameter to the callback
 void mqttsn_gw_set_tick_req_cb(MqttsnGatewayHandle gw, MqttsnGwTickReqCb cb, void* data);
+
+/// @brief Set callback that requests to send serialised @b ADVERTISE message.
+/// @param[in] gw Handle returned by mqttsn_gw_alloc() function.
+/// @param[in] cb Pointer to callback function
+/// @param[in] data Any user data, to be passed as first parameter to the callback
 void mqttsn_gw_set_advertise_broadcast_req_cb(
     MqttsnGatewayHandle gw,
     MqttsnGwBroadcastReqCb cb,
     void* data);
+
+/// @brief Start operation of the @b Gateway object.
+/// @details The function check whether all callbacks and proper advertise
+///     period were set properly and start the operation if everything is in
+///     place. If start is successful, the callbacks of requesting data send
+///     as well as data measurement request will be invoked.
+/// @param[in] gw Handle returned by mqttsn_gw_alloc() function.
+/// @return @b true if the operation was successfully started, @b false otherwise.
 bool mqttsn_gw_start(MqttsnGatewayHandle gw);
+
+/// @brief Stop operation of the @b Gateway object.
+/// @param[in] gw Handle returned by mqttsn_gw_alloc() function.
 void mqttsn_gw_stop(MqttsnGatewayHandle gw);
+
+/// @brief Notify the @ref Gateway object about requested time expiry.
+/// @details Invocation of this function will cause  invocation of the send
+///     data request callback as well as new time measurement request.
+/// @param[in] gw Handle returned by mqttsn_gw_alloc() function.
 void mqttsn_gw_tick(MqttsnGatewayHandle gw);
 
 MqttsnSessionHandle mqttsn_gw_session_alloc(void);
