ee24b7b37b584197cec05b4c5ba8fa279d9148ea
Alex Robenko <arobenko@gmail.com>
2018-03-23 16:22:57 +1000

    Updating protocol stack definition.

--- protocol/include/mqttsn/protocol/Stack.h
+++ protocol/include/mqttsn/protocol/Stack.h
@@ -32,14 +32,15 @@ namespace protocol
 template <
     typename TMsgBase = Message,
     typename TAllMessages = AllMessages<TMsgBase>,
-    typename TMsgAllocOptions = std::tuple<> >
+    typename TMsgAllocOptions = comms::option::EmptyOption,
+    typename TDataFieldStorageOptions = comms::option::EmptyOption>
 using Stack =
     MsgSizeLayer<
         comms::protocol::MsgIdLayer<
             field::MsgType<typename TMsgBase::Field>,
             TMsgBase,
             TAllMessages,
-            comms::protocol::MsgDataLayer<>,
+            comms::protocol::MsgDataLayer<TDataFieldStorageOptions>,
             TMsgAllocOptions
         >
     >;
