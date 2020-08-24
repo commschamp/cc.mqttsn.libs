d8e3ef8438edceb71b66e2aa407303678dd5b4d7
Alex Robenko <arobenko@gmail.com>
2020-08-25 08:10:06 +1000

    Using bionic distro in travis-ci + testing "Visual Studio 2019" in appveyor.

--- script/appveyor_install.bat
+++ script/appveyor_install.bat
@@ -1,6 +1,7 @@
 IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2015" (
     set TOOLCHAIN=msvc14
     set QT_SUBDIR=msvc2015
+    set QT_VER=5.6
     IF "%PLATFORM%"=="x86" (
         echo Performing x86 build in VS2015
         call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
@@ -11,6 +12,7 @@ IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2015" (
 ) ELSE IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2017" (
     set TOOLCHAIN=msvc15
     set QT_SUBDIR=msvc2017
+    set QT_VER=5.11
     IF "%PLATFORM%"=="x86" (
         echo Performing x86 build in VS2017
         call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
@@ -18,22 +20,34 @@ IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2015" (
         echo Performing amd64 build in VS2017
         call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
     )
+    
+) ELSE IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2019" (
+    set TOOLCHAIN=msvc16
+    set QT_SUBDIR=msvc2017
+    set QT_VER=5.14
+    IF "%PLATFORM%"=="x86" (
+        echo Performing x86 build in VS2019
+        call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat"
+    ) ELSE (
+        echo Performing amd64 build in VS2019
+        call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+    )
 ) ELSE (
     echo Toolchain %TOOLCHAIN% is not supported
     exit -1
 )
 
-set QTDIR_PREFIX=C:\Qt\%QT_VER%
+set QTDIR_PREFIX=C:/Qt/%QT_VER%
 IF "%PLATFORM%"=="x86" (
     set QTDIR_SUFFIX=
 ) ELSE (
     set QTDIR_SUFFIX=_64
 )
 
-set QTDIR=%QTDIR_PREFIX%\%QT_SUBDIR%%QTDIR_SUFFIX%
+set QTDIR=%QTDIR_PREFIX%/%QT_SUBDIR%%QTDIR_SUFFIX%
 IF NOT EXIST %QTDIR% (
     echo WARNING: %QTDIR% does not exist!!!
-    set QTDIR=%QTDIR_PREFIX%\msvc2015%QTDIR_SUFFIX%
+    set QTDIR=%QTDIR_PREFIX%/msvc2015%QTDIR_SUFFIX%
 )
 
 echo Using Qt5 from %QTDIR%
