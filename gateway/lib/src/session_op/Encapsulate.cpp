37778fe13b1fa22347d9a76b820ebecca0d57968
Alex Robenko <arobenko@gmail.com>
2026-01-09 08:32:56 +1000

    Updated copyright years to include 2026.

--- gateway/lib/src/session_op/Encapsulate.cpp
+++ gateway/lib/src/session_op/Encapsulate.cpp
@@ -1,5 +1,5 @@
 //
-// Copyright 2024 - 2025 (C). Alex Robenko. All rights reserved.
+// Copyright 2024 - 2026 (C). Alex Robenko. All rights reserved.
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
