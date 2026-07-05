381ad376aa4a4a162e76c53569368c6ba80b2648
Alex Robenko <arobenko@gmail.com>
2026-07-06 09:47:13 +1000

    Updating next release version to be v2.0.13

--- client/lib/include/cc_mqttsn_client/common.h
+++ client/lib/include/cc_mqttsn_client/common.h
@@ -28,7 +28,7 @@ extern "C" {
 
 /// @brief Patch level of the library
 /// @ingroup global
-#define CC_MQTTSN_CLIENT_PATCH_VERSION 12U
+#define CC_MQTTSN_CLIENT_PATCH_VERSION 13U
 
 /// @brief Macro to create numeric version as single unsigned number
 #define CC_MQTTSN_CLIENT_MAKE_VERSION(major_, minor_, patch_) \
